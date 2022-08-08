6fc1722c1112da57ee12c07effed07c59bcfafb6
Dolzy <dolzy@LAPTOP-SOLC3NM9.localdomain>
2022-08-08 01:41:22 +0100

    refactored base_model

---
+++ models/base_model.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+""" A base model for all objects of the project"""
+import uuid
+import cmd
+from datetime import datetime
+
+class BaseModel:
+    """
+    A class that defines all common attributes/methods for
+    other classes
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialiazes the Base model atributes"""
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    pass
+                elif key == "created_at":
+                    self.created_at = datetime.fromisoformat(value)
+                elif key == "updated_at":
+                    self.updated_at = datetime.fromisoformat(value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """A string representation of the Base Model"""
+        return '[{}] ({}) <{}>'.format(
+                self.__class__.__name__,
+                self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at with the
+        current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all key/values of __dict__
+        of the instance
+        """
+        new_dict = self.__dict__
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return (new_dict)
+    
---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""A File storage class"""
+import json
+
+class FileStorage:
+    """
+    A class representing a File Storage System
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return (self.__objects)
+    
+    def new(self, obj):
+        pass
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file path: __file_path
+        """
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
+            json.dump(self.__objects, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, 'r', encoding="utf-8") as f:
+                self.__objects = json.load(f)
+        except FileNotFoundError:
+            pass
