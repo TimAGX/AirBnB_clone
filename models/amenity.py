e7d14a8ecf20c66cb19a5f4002a9632489419cc5
Dolzy <dolzy@LAPTOP-SOLC3NM9.localdomain>
2022-08-08 02:12:16 +0100

    added classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Contains the Amenity model"""
-from models.base_model import BaseModel
+from base_model import BaseModel
 
 
 class Amenity(BaseModel):
--- models/city.py
+++ models/city.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Contains the City model"""
-from models.base_model import BaseModel
+from base_model import BaseModel
 
 
 class City(BaseModel):
     """Implements the City class"""
     state_id = ""
     name = ""
-    
